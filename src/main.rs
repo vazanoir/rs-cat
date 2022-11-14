92950098cd0df596285df5066687e4631155eaca
Vazanoir <vazanoir@outlook.fr>
2022-11-14 19:18:38 +0100

    Print file if not an option

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Please enter a path");
+        return;
+    }
+
+    for arg in &args[1.. ] {
+        if arg.chars().nth(0).unwrap() != '-' {
+            let contents = fs::read_to_string(arg)
+                .expect("Please use valid path");
+
+            println!("{contents}");
+        }
+    }
 }
