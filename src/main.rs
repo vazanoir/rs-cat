e64035e26b5bb35e372eafc6176a65ded8cc41b6
Vazanoir <vazanoir@outlook.fr>
2022-11-14 19:39:16 +0100

    Add -E option

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::fs;
+use std::io;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,12 +11,31 @@ fn main() {
         return;
     }
 
+    let mut show_ends = false;
+
+    for arg in &args[1.. ] {
+        if arg == "-E" {
+            show_ends = true;
+        }
+    }
+
     for arg in &args[1.. ] {
         if arg.chars().nth(0).unwrap() != '-' {
-            let contents = fs::read_to_string(arg)
-                .expect("Please use valid path");
+                let file = fs::File::open(arg).expect("Please enter a valid file");
+                let buf_reader = io::BufReader::new(file);
 
-            println!("{contents}");
+                for line in buf_reader.lines() {
+                    match line {
+                        Ok(mut line) => {
+                            if show_ends == true {
+                                line = line + "$";
+                            }
+
+                            println!("{line}")
+                        },
+                        Err(e) => println!("{e}"),
+                    }
+                }
         }
     }
 }
